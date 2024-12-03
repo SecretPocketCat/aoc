09c5128abc56ab4b788422554ff86c26eedfd7fe
Jiří H. <jiri@bacehub.de>
2024-12-03 23:25:47 +0100

    feat: day 3 part b

--- 2024/rust/day-3/src/lib.rs
+++ 2024/rust/day-3/src/lib.rs
@@ -2,7 +2,7 @@ pub mod solution {
     #[tracing::instrument(fields(input = format!("{:?}[...]", input.lines().next())))]
     pub fn part_a(input: &str) -> anyhow::Result<String> {
         let rgx = regex::Regex::new(r"^(\d+),(\d+)\)").expect("Valid regex");
-        let count: u64 = input
+        let sum: u64 = input
             .split("mul(")
             .filter_map(|s| match rgx.captures(s) {
                 Some(captures) => match (captures.get(1), captures.get(2)) {
@@ -19,12 +19,43 @@ pub mod solution {
                 None => None,
             })
             .sum();
-        Ok(count.to_string())
+        Ok(sum.to_string())
     }
 
     #[tracing::instrument(fields(input = format!("{:?}[...]", input.lines().next())))]
+
     pub fn part_b(input: &str) -> anyhow::Result<String> {
-        todo!("b")
+        let rgx = regex::Regex::new(r"^(\d+),(\d+)\)").expect("Valid regex");
+        let sum_muls = |s: &str| {
+            s.split("mul(")
+                .filter_map(|s| match &rgx.captures(s) {
+                    Some(captures) => match (captures.get(1), captures.get(2)) {
+                        (Some(a), Some(b)) => Some(
+                            a.as_str()
+                                .parse::<u64>()
+                                .expect("Valid digit matched by regex")
+                                * b.as_str()
+                                    .parse::<u64>()
+                                    .expect("Valid digit matched by regex"),
+                        ),
+                        _ => None,
+                    },
+                    None => None,
+                })
+                .sum::<u64>()
+        };
+        let sum: u64 = input
+            .split("don't()")
+            .enumerate()
+            .filter_map(|(i, s)| match i {
+                0 => Some(sum_muls(s)),
+                _ => match s.split_once("do()") {
+                    Some((_, s)) => Some(sum_muls(s)),
+                    None => None,
+                },
+            })
+            .sum();
+        Ok(sum.to_string())
     }
 }
 
@@ -32,19 +63,18 @@ pub mod solution {
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = include_str!("../inputs/example.txt");
     const EXPECTED_A: &str = "161";
-    const EXPECTED_B: &str = "todo_expected_b";
+    const EXPECTED_B: &str = "48";
 
     #[test]
     fn day_3_a() {
-        let res = solution::part_a(TEST_INPUT);
+        let res = solution::part_a(include_str!("../inputs/example_a.txt"));
         assert_eq!(EXPECTED_A, res.unwrap());
     }
 
     #[test]
     fn day_3_b() {
-        let res = solution::part_b(TEST_INPUT);
+        let res = solution::part_b(include_str!("../inputs/example_b.txt"));
         assert_eq!(EXPECTED_B, res.unwrap());
     }
 }
