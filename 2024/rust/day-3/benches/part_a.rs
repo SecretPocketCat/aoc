fe94335beba3683f7b0d749d320a84a0090125d8
SecretPocketCat <jiridexterhanc@gmail.com>
2024-12-03 23:27:34 +0100

    feat: Day 3 (#1)
    * feat: day 3 part a
    
    * feat: day 3 part b
    
    ---------
    
    Co-authored-by: Jiří H. <jiri@bacehub.de>

---
+++ 2024/rust/day-3/benches/part_a.rs
@@ -0,0 +1,12 @@
+use aoc_client::block_on_input;
+use criterion::{criterion_group, criterion_main, Criterion};
+use day_3::solution::part_a;
+use std::hint::black_box;
+
+fn part_a_benchmark(c: &mut Criterion) {
+    let input = block_on_input(3);
+    c.bench_function("part_a", |b| b.iter(|| part_a(black_box(&input))));
+}
+
+criterion_group!(benches, part_a_benchmark);
+criterion_main!(benches);
