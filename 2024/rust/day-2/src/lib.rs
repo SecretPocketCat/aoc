5af1a20bdf800d84abc8d04c5fb6e7684f114b67
Jiří H. <jiri@bacehub.de>
2024-12-03 22:33:03 +0100

    feat: day 2 part b WIP

--- 2024/rust/day-2/src/lib.rs
+++ 2024/rust/day-2/src/lib.rs
@@ -4,13 +4,12 @@ pub mod solution {
         let count = input
             .lines()
             .filter(|l| {
-                let chars: Vec<_> = l
+                let nums: Vec<_> = l
                     .split_whitespace()
-                    .map(|num| num.parse::<i64>())
-                    .flatten()
+                    .flat_map(|num| num.parse::<i64>())
                     .collect();
                 let mut inc = None;
-                chars.windows(2).all(|nums| {
+                nums.windows(2).all(|nums| {
                     let delta = nums[1] - nums[0];
                     let sign = delta > 0;
                     match inc {
@@ -32,7 +31,63 @@ pub mod solution {
 
     #[tracing::instrument(fields(input = format!("{:?}[...]", input.lines().next())))]
     pub fn part_b(input: &str) -> anyhow::Result<String> {
-        todo!("b")
+        fn report_valid(nums: Vec<i64>, dampened: bool) -> bool {
+            let mut sign = None;
+            let mut i = 0;
+            while let Some(b) = nums.get(i + 1).cloned() {
+                let a = nums[i];
+                let delta = b - a;
+                let mut invalid = false;
+                if !(1..=3).contains(&delta.abs()) {
+                    invalid = true;
+                } else {
+                    let curr_sign = delta.signum();
+                    match sign {
+                        Some(sign) if sign != curr_sign => {
+                            invalid = true;
+                        }
+                        None => {
+                            sign = Some(curr_sign);
+                        }
+                        _ => {}
+                    }
+                }
+
+                match (invalid, dampened) {
+                    (true, true) => {
+                        return false;
+                    }
+                    (true, false) => {
+                        let mut n1 = nums.clone();
+                        n1.remove(i);
+                        let mut n2 = nums.clone();
+                        n2.remove(i + 1);
+                        let res = report_valid(n1, true) || report_valid(n2, true);
+                        // if !res {
+                        //     tracing::warn!(?nums, a, b, "nope");
+                        // }
+                        return res;
+                    }
+                    _ => {
+                        i += 1;
+                    }
+                }
+            }
+
+            true
+        }
+
+        let count = input
+            .lines()
+            .filter(|l| {
+                let nums: Vec<_> = l
+                    .split_whitespace()
+                    .flat_map(|num| num.parse::<i64>())
+                    .collect();
+                report_valid(nums, false)
+            })
+            .count();
+        Ok(count.to_string())
     }
 }
 
@@ -42,7 +97,7 @@ mod tests {
 
     const TEST_INPUT: &str = include_str!("../inputs/example.txt");
     const EXPECTED_A: &str = "2";
-    const EXPECTED_B: &str = "todo_expected_b";
+    const EXPECTED_B: &str = "4";
 
     #[test]
     fn day_2_a() {
@@ -54,5 +109,17 @@ mod tests {
     fn day_2_b() {
         let res = solution::part_b(TEST_INPUT);
         assert_eq!(EXPECTED_B, res.unwrap());
+    }
+
+    #[test]
+    fn day_2_b_repro_1() {
+        let res = solution::part_b("72 73 76 75 77 80 81");
+        assert_eq!("1", res.unwrap());
+    }
+
+    #[test]
+    fn day_2_b_repro_2() {
+        let res = solution::part_b("85 86 87 85");
+        assert_eq!("1", res.unwrap());
     }
 }
