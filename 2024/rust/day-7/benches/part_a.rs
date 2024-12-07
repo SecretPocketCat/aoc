a760910d1ecc45c8e0ee4e2ac94239450b25aaff
Jiří H. <jiri@bacehub.de>
2024-12-07 13:56:47 +0100

    bench: day 7 - update benches

--- 2024/rust/day-7/benches/part_a.rs
+++ 2024/rust/day-7/benches/part_a.rs
@@ -5,7 +5,9 @@ use std::hint::black_box;
 
 fn part_a_benchmark(c: &mut Criterion) {
     let input = block_on_input(7);
-    c.bench_function("part_a", |b| b.iter(|| part_a(black_box(&input))));
+    let mut group = c.benchmark_group("day-7");
+    group.bench_function("part_a", |b| b.iter(|| part_a(black_box(&input))));
+    group.finish();
 }
 
 criterion_group!(benches, part_a_benchmark);
