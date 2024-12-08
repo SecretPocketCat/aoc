f60379f8898ec6305e9c76297cde9af30a70a6e1
Jiří H. <jiri@bacehub.de>
2024-12-08 13:29:35 +0100

    feat: day 6 part b

--- 2024/rust/day-5/src/lib.rs
+++ 2024/rust/day-5/src/lib.rs
@@ -1,10 +1,10 @@
-// struct OrderedNum {
-//     after: HashSet<u8>,
-//     before: HashSet<u8>,
-// }
-
 pub mod solution {
-    use std::collections::{HashMap, HashSet};
+    use std::{
+        cmp::Ordering,
+        collections::{HashMap, HashSet},
+    };
+
+    type RulePair = (u32, u32);
 
     #[tracing::instrument(fields(input = format!("{:?}[...]", input.lines().next())))]
     pub fn part_a(input: &str) -> anyhow::Result<String> {
@@ -50,9 +50,80 @@ pub mod solution {
         Ok(res.to_string())
     }
 
-    #[tracing::instrument(fields(input = format!("{:?}[...]", input.lines().next())))]
+    #[tracing::instrument(skip(input))]
     pub fn part_b(input: &str) -> anyhow::Result<String> {
-        todo!("b")
+        let (processed_line_count, rules) = parse_rules(input);
+
+        let res: u32 = input
+            .lines()
+            .skip(processed_line_count)
+            .filter_map(|l| {
+                let pages: Vec<u32> = l.split(',').flat_map(str::parse).collect();
+                eval_update(&pages, &rules)
+            })
+            .sum();
+        Ok(res.to_string())
+    }
+
+    #[tracing::instrument(skip(input))]
+    pub fn parse_rules(input: &str) -> (usize, Vec<RulePair>) {
+        let mut rules = Vec::new();
+        for line in input.lines().skip_while(|l| l.is_empty()) {
+            match line.split_once('|') {
+                Some((a, b)) => {
+                    let a: u32 = a.parse().expect("Valid number a");
+                    let b: u32 = b.parse().expect("Valid number b");
+                    rules.push((a, b));
+                }
+                None => {
+                    break;
+                }
+            }
+        }
+        (rules.len() + 1, rules)
+    }
+
+    fn eval_update(pages: &[u32], all_rules: &[RulePair]) -> Option<u32> {
+        let page_set: HashSet<_> = pages.iter().copied().collect();
+        let rule_pairs: Vec<_> = all_rules
+            .iter()
+            .filter(|(a, b)| page_set.contains(a) || page_set.contains(b))
+            .collect();
+        let mut rule_map: HashMap<u32, HashSet<RulePair>> = HashMap::new();
+        for rule in rule_pairs {
+            rule_map
+                .entry(rule.0)
+                .and_modify(|r| {
+                    r.insert(*rule);
+                })
+                .or_insert_with(|| [*rule].into());
+            rule_map
+                .entry(rule.1)
+                .and_modify(|r| {
+                    r.insert(*rule);
+                })
+                .or_insert_with(|| [*rule].into());
+        }
+        let mut sorted_pages = pages.to_vec();
+        sorted_pages.sort_unstable_by(|a, b| {
+            let rules = &rule_map[a];
+            let rule = rules
+                .iter()
+                .find(|rule| rule.0 == *b || rule.1 == *b)
+                // todo: this probly won't work
+                .expect("Matching rule");
+            if rule.0 == *a {
+                Ordering::Less
+            } else {
+                Ordering::Greater
+            }
+        });
+
+        if sorted_pages.iter().enumerate().all(|(i, p)| *p == pages[i]) {
+            None
+        } else {
+            Some(sorted_pages[sorted_pages.len() / 2])
+        }
     }
 }
 
@@ -63,7 +134,7 @@ mod tests {
 
     const TEST_INPUT: &str = include_str!("../inputs/example.txt");
     const EXPECTED_A: &str = "143";
-    const EXPECTED_B: &str = "todo_expected_b";
+    const EXPECTED_B: &str = "123";
 
     #[test]
     #[traced_test]
