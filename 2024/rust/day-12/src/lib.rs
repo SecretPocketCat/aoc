4f727868b435c35e35a411ffe356fcecfd44172c
Jiří H. <jiri@bacehub.de>
2024-12-17 21:50:13 +0100

    feat: day 12 part b

--- 2024/rust/day-12/src/lib.rs
+++ 2024/rust/day-12/src/lib.rs
@@ -1,12 +1,14 @@
 pub mod solution {
-    use std::collections::HashMap;
+    use std::collections::{HashMap, HashSet};
 
     use glam::{IVec2, UVec2};
+    use itertools::Itertools;
     use tracing::warn;
 
     const DIRS: [IVec2; 4] = [IVec2::NEG_Y, IVec2::X, IVec2::Y, IVec2::NEG_X];
 
     struct NeighbourCount(usize);
+
     type VisitedMap = HashMap<UVec2, NeighbourCount>;
 
     #[derive(Debug)]
@@ -32,7 +34,7 @@ pub mod solution {
                 return None;
             }
             let target = target.as_uvec2();
-            self.0.get(&target).map(|height| (target, *height))
+            self.0.get(&target).map(|c| (target, *c))
         }
 
         fn drain_area(&mut self, pos: UVec2) -> VisitedMap {
@@ -79,8 +81,50 @@ pub mod solution {
 
     #[tracing::instrument(skip(input))]
     pub fn part_b(input: &str) -> anyhow::Result<String> {
-        let mut _map = Map::new(input);
-        todo!("b")
+        let mut map = Map::new(input);
+        let mut price = 0;
+        while let Some(pos) = map.0.keys().next() {
+            let visited = map.drain_area(*pos);
+            let visited: HashSet<_> = visited.keys().map(|t| t.as_ivec2()).collect();
+            let area = visited.len();
+            let edges: usize = visited
+                .iter()
+                // sort the tiles from top-left to btm-right
+                .sorted_unstable_by(|a, b| a.y.cmp(&b.y).then(a.x.cmp(&b.x)))
+                .map(|tile| {
+                    DIRS.iter()
+                        .filter(|dir| {
+                            let target = tile + *dir;
+                            match visited.get(&target) {
+                                // region tile in that direction => no edge
+                                Some(_) => false,
+                                _ => match **dir {
+                                    IVec2::NEG_Y => {
+                                        !visited.contains(&(tile + IVec2::NEG_X))
+                                            || visited.contains(&(tile + IVec2::NEG_ONE))
+                                    }
+                                    IVec2::Y => {
+                                        !visited.contains(&(tile + IVec2::NEG_X))
+                                            || visited.contains(&(tile + IVec2::new(-1, 1)))
+                                    }
+                                    IVec2::NEG_X => {
+                                        !visited.contains(&(tile + IVec2::NEG_Y))
+                                            || visited.contains(&(tile + IVec2::NEG_ONE))
+                                    }
+                                    IVec2::X => {
+                                        !visited.contains(&(tile + IVec2::NEG_Y))
+                                            || visited.contains(&(tile + IVec2::new(1, -1)))
+                                    }
+                                    _ => unreachable!(),
+                                },
+                            }
+                        })
+                        .count()
+                })
+                .sum();
+            price += area * edges;
+        }
+        Ok(price.to_string())
     }
 }
 
@@ -91,7 +135,7 @@ mod tests {
 
     const TEST_INPUT: &str = include_str!("../inputs/example.txt");
     const EXPECTED_A: &str = "140";
-    const EXPECTED_B: &str = "todo_expected_b";
+    const EXPECTED_B: &str = "80";
 
     #[test]
     #[traced_test]
@@ -105,5 +149,13 @@ mod tests {
     fn day_12_b() {
         let res = solution::part_b(TEST_INPUT);
         assert_eq!(EXPECTED_B, res.unwrap());
+    }
+
+    #[test]
+    #[traced_test]
+    #[allow(non_snake_case)]
+    fn day_12_b_E_map() {
+        let res = solution::part_b(include_str!("../inputs/example_E_map.txt"));
+        assert_eq!("236", res.unwrap());
     }
 }
